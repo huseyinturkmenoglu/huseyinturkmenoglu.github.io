10b05b363e63a0ec959a264558beca9fe018fba4
HuseyinTurkmenoglu <huseyin.turkmenoglu@obss.com.tr>
2023-01-22 20:48:29 +0300

    remove out

--- out/_next/static/dqjmux6-M86StfIebExTA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8655752d24a80f54.css","static/chunks/pages/index-ae689f62f3474925.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
