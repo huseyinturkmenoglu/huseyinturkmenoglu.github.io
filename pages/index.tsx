1de972cc4ba59c0d5f689dbd4bd443d8670ac764
Hüseyin Türkmenoğlu <turkmenogluhuseyin@gmail.com>
2023-01-23 00:05:53 +0300

    changed URL

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
           </p>
           <div>
             <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              href="https://www.linkedin.com/in/huseyinturkmenoglu/"
               target="_blank"
               rel="noopener noreferrer"
             >
